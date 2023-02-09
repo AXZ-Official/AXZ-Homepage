30eb195ad316e4003d8a34e9ea1edbc2e59a0ca9
Probmkr <69635586+Probmkr@users.noreply.github.com>
2023-02-09 04:28:07 +0000

    プライバシーポリシーを追加 サーバーに参加ページを作成

---
+++ src/pages/join.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import Layout from "../lib/layout";
+
+const TemplatePage: FC<{}> = () => {
+  return (
+    <Layout>
+      <h1>サーバーに参加！</h1>
+      <p></p>
+    </Layout>
+  );
+};
+
+export default TemplatePage;
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import Layout from "../lib/layout";
+import Link from "next/link";
+
+const TemplatePage: FC<{}> = () => {
+  return (
+    <Layout>
+      <h1>プライバシーポリシー</h1>
+      <p>当サイトでは、アクセス解析サービス「Googleアナリティクス」を使用しています。</p>
+      <p>Googleアナリティクスはデータの収集のためにCookieを使用しています。</p>
+      <p>このデータは匿名で収集されており、個人を特定するものではありません</p>
+      <p>この機能はCookieを無効にすることで拒否することができますので、お使いのブラウザの設定をご確認ください。</p>
+      <p>この規約に関しての詳細は<Link href="https://marketingplatform.google.com/about/analytics/terms/jp/" target="_blank">Google アナリティクス利用規約</Link>のページや<Link href="https://policies.google.com/technologies/ads?hl=ja">Google ポリシーと規約</Link>のページをご覧ください。</p>
+    </Layout>
+  );
+};
+
+export default TemplatePage;
